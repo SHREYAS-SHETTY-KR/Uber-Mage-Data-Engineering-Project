2de8c6e855631f276f94e47ec6d7e450ee18b2d8
Shreyas <79562771+Shreyas-028@users.noreply.github.com>
2023-11-05 18:59:01 +0530

    Update transform.py

--- Mage-files/transform.py
+++ Mage-files/transform.py
@@ -21,6 +21,9 @@ def transform(df, *args, **kwargs):
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your transformation logic here
+    df = df.drop_duplicates().reset_index(drop=True)
+    df['trip_id'] = df.index
+    
     df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
     df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 
